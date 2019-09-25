9e60e56c41ab8efa29b0c909dda2096236273508
Jacky Lau <jackylau0126@gmail.com>
2019-09-25 22:26:01 +0000

    Impletemented setupInput and exit function; pair programming with Brandon

--- play.js
+++ play.js
@@ -1,3 +1,23 @@
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = function (data) {
+    if (data === '\u0003') {
+      process.exit();
+    }
+  }
+  stdin.on('data', handleUserInput);
+}
+
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+setupInput();
