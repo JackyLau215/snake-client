23105fb58f57bd5109461245e0a613703ab8c884
Jacky Lau <jackylau0126@gmail.com>
2019-09-25 22:32:05 +0000

    input module; pair programming with Brandon

--- play.js
+++ play.js
@@ -2,21 +2,11 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
 
-  const handleUserInput = function (data) {
-    if (data === '\u0003') {
-      process.exit();
-    }
-  }
-  stdin.on('data', handleUserInput);
-}
 
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
 console.log('Connecting ...');
 connect();
 
